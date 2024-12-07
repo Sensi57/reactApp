03e78c351a8930fc093bfa2768dcf20537c6e3c0
Falko <you@example.com>
2024-12-07 21:36:28 +0500

    replay

--- src/app/providers/router/config/routeConfig.tsx
+++ src/app/providers/router/config/routeConfig.tsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import { AuthRoute } from "../ui/AuthRoute";
 import MainLayout from "../../layouts/MainLayout/MainLayout";
-import { ShopPage } from "../../../../pages/ShopPage/ShopPage";
+import { ROUTES } from "../constants/routes";
+
+import {  ShopPage, 
+          NotFoundPage
+       } from "../../../../pages";
 
 
 export const router = createBrowserRouter([
@@ -17,6 +21,10 @@ export const router = createBrowserRouter([
                 index: true,
                 element: <ShopPage />
             }
-        ]
+        ],
+    },
+    {
+        path: ROUTES.page_not_fount.route,
+        element: <NotFoundPage />
     }
 ])
